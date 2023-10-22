4b56bd2899ad91811f0c3fe48c6222d42e12dac9
NickSettler <moiseevnikita14@gmail.com>
2023-10-22 19:10:12 +0200

    Initial Commit

---
+++ ITU/ITUApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITUApp.swift
+//  ITU
+//
+//  Created by Никита Моисеев on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ITUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
