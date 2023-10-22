7f2f3ec0ea193b094460eb01584a165bfe682a60
NickSettler <moiseevnikita14@gmail.com>
2023-10-22 20:38:01 +0200

    feat(extensions): add view extension

--- ITU/extensions/View.swift
+++ ITU/extensions/View.swift
@@ -5,4 +5,28 @@
 //  Created by Никита Моисеев on 22.10.2023.
 //
 
-import Foundation
+import SwiftUI
+
+/// Custom SwiftUI View Extensions
+extension View {
+    /// View Alignments
+    @ViewBuilder
+    func hSpacing(_ alignment: Alignment = .center) -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    @ViewBuilder
+    func vSpacing(_ alignment: Alignment = .center) -> some View {
+        self
+            .frame(maxHeight: .infinity, alignment: alignment)
+    }
+    
+    /// Disable With Opacity
+    @ViewBuilder
+    func disableWithOpacity(_ condition: Bool) -> some View {
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.5 : 1)
+    }
+}
