f58a1bf8a0ffa1ee6c94fb7edc9fedbd53fe0535
NickSettler <moiseevnikita14@gmail.com>
2023-11-04 14:37:27 +0100

    refactor(auth): remove prefilled credentials

--- ITU/views/auth/LoginView/LoginViewModel.swift
+++ ITU/views/auth/LoginView/LoginViewModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import SwiftUI
 
 @MainActor class LoginViewModel : ObservableObject {
-    @Published var email: String = "test@settler.tech"
-    @Published var password: String = "PassWord"
+    @Published var email: String = ""
+    @Published var password: String = ""
     
     @AppStorage(E_AUTH_STORAGE_KEYS.ACCESS_TOKEN.rawValue) var access_token: String?
     
