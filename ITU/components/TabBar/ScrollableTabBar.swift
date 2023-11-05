c9dd2725f663eed70674fe6cc64297d5d8b79167
NickSettler <moiseevnikita14@gmail.com>
2023-11-05 01:48:40 +0100

    feat(components): add scrollable tab bar component

--- ITU/components/TabBar/ScrollableTabBar.swift
+++ ITU/components/TabBar/ScrollableTabBar.swift
@@ -7,12 +7,128 @@
 
 import SwiftUI
 
-struct ScrollableTabBar: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ScrollableTabBar<Content: View>: UIViewRepresentable {
+
+    // to store our SwiftUI View...
+    var content: Content
+    
+    // Getting Rect To Calculate Width And Height Of ScrollView...
+    
+    var rect: CGRect
+    
+    // ContentOffset...
+    @Binding var offset: CGFloat
+    
+    // Tabs...
+    var tabs: [Any]
+    
+    // ScrollView...
+    // For Paging AKA Scrollable Tabs...
+    let scrollView = UIScrollView()
+    
+    init(
+        tabs: [Any],
+        rect: CGRect,
+        offset: Binding<CGFloat>,
+        @ViewBuilder content: ()->Content
+    ) {
+        
+        self.content = content()
+        self._offset = offset
+        self.rect = rect
+        self.tabs = tabs
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return ScrollableTabBar.Coordinator(parent: self)
+    }
+    
+    func makeUIView(context: Context) ->  UIScrollView {
+        
+        setUpScrollView()
+        // setting Content Size...
+        
+        scrollView.contentSize = CGSize(width: rect.width * CGFloat(tabs.count), height: rect.height)
+        
+        scrollView.addSubview(extractView())
+        
+        scrollView.delegate = context.coordinator
+        
+        return scrollView
+    }
+    
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+        
+        // Updating View...
+        if uiView.contentOffset.x != offset{
+            
+            // Animating...
+            // The ANimation Glitch Is Because It s Updating On Two Times...
+
+            // Simple....
+            // Removing Delegate While Animating...
+            
+            uiView.delegate = nil
+            
+            UIView.animate(withDuration: 0.4) {
+                uiView.contentOffset.x = offset
+            } completion: { (status) in
+                if status{uiView.delegate = context.coordinator}
+            }
+
+        }
+    }
+    
+    // setting Up ScrollView...
+    func setUpScrollView(){
+        scrollView.isPagingEnabled = true
+        scrollView.bounces = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+    // Extracting SwiftUI View...
+    func extractView()->UIView{
+        
+        // since it depends upon tab size..
+        // so we getting tabs also...
+        
+        let controller = UIHostingController(rootView: content)
+        controller.view.frame = CGRect(x: 0, y: 0, width: rect.width * CGFloat(tabs.count), height: rect.height)
+        
+        return controller.view!
+    }
+    
+    // Delegate Function To Get Offset...
+    
+    class Coordinator: NSObject,UIScrollViewDelegate{
+        
+        var parent: ScrollableTabBar
+        
+        init(parent: ScrollableTabBar) {
+            self.parent = parent
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            parent.offset = scrollView.contentOffset.x
+        }
     }
 }
 
 #Preview {
-    ScrollableTabBar()
+    GeometryReader{proxy in
+        let rect = proxy.frame(in: .global)
+        
+        ScrollableTabBar(
+            tabs: tabs,
+            rect: rect,
+            offset: .constant(0)
+        ) {
+            HStack(spacing: 0){
+                ForEach(tabs, id: \.self){ tab in
+                    Text(tab)
+                }
+            }
+        }
+    }
 }
--- ITU/components/TabBar/TabBar.swift
+++ ITU/components/TabBar/TabBar.swift
@@ -8,11 +8,96 @@
 import SwiftUI
 
 struct TabBar: View {
+    @Binding var offset: CGFloat
+    @State var width : CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ proxy -> AnyView in
+            let equalWidth = proxy.frame(in: .global).width / 4
+            
+            DispatchQueue.main.async {
+                self.width = equalWidth
+            }
+            
+            return AnyView(
+                ScrollViewReader { scrollView in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        ZStack(alignment: .bottomLeading, content: {
+                            Capsule()
+                                .fill(.colorPrimaryLight)
+                                .frame(
+                                    width: abs(equalWidth - 15),
+                                    height: 4
+                                )
+                                .offset(x: getOffset() + 7)
+                            
+                            HStack(spacing: 0) {
+                                ForEach(tabs.indices,id: \.self) { index in
+                                    Text(tabs[index])
+                                        .fontWeight(.bold)
+                                        .foregroundColor(
+                                            getIndexFromOffset() == CGFloat(index)
+                                            ? Color.colorPrimaryLight
+                                            : Color.textColorSecondary
+                                        )
+                                        .frame(
+                                            width: equalWidth,
+                                            height: 40
+                                        )
+                                        .contentShape(Rectangle())
+                                        .onTapGesture {
+                                            withAnimation {
+                                                offset = UIScreen.main.bounds.width * CGFloat(index)
+                                            }
+                                        }
+                                        .id(index)
+                                    
+                                }
+                            }
+                            
+                        })
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: 40,
+                            alignment: .center
+                        )
+                    }
+                    .onChange(of: offset) {
+                        withAnimation {
+                            scrollView.scrollTo(getIndexFromOffset())
+                        }
+                    }
+                }
+            )
+        }
+        .frame(height: 40)
+    }
+    
+    func getOffset()->CGFloat{
+        let progress = offset / UIScreen.main.bounds.width
+        
+        return progress * width
+    }
+    
+    func getIndexFromOffset()->CGFloat{
+        let indexFloat = offset / UIScreen.main.bounds.width
+        
+        return indexFloat.rounded(.toNearestOrAwayFromZero)
     }
 }
 
 #Preview {
-    TabBar()
+    var offset: CGFloat = 0
+    let offsetBinding: Binding<CGFloat> = .init(
+        get: {
+            return offset
+        },
+        set: { off in
+            offset = off
+        }
+    )
+    
+    return TabBar(
+        offset: offsetBinding
+    )
 }
