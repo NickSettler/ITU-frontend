9c3139a69e9046b2d048bb3ff4f83d06bae94e74
NickSettler <moiseevnikita14@gmail.com>
2023-11-05 17:42:58 +0100

    feat(api): update folders service

--- ITU/api/services/FoldersService.swift
+++ ITU/api/services/FoldersService.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 struct FoldersService {
+    static func getFolders() async -> ApiSuccessResponse<GetAllUserFoldersResponse>? {
+        try? await AuthService.conditionalRefresh()
+        
+        do {
+            let data = try await NetworkManager.shared.get(
+                path: "/items/user_locations",
+                parameters: nil
+            )
+            
+            let result: ApiSuccessResponse<GetAllUserFoldersResponse> = try NetworkAPI.parseData(data: data)
+            
+            return result
+        } catch let error {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
     static func createFolder(name: String) async -> Bool {
         try? await AuthService.conditionalRefresh()
         
@@ -16,6 +34,40 @@ struct FoldersService {
                 path: "/items/user_locations",
                 parameters: [
                     "name": name
+                ]
+            )
+            
+            return true
+        } catch let error {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    static func deleteFolder(id: String) async -> Bool {
+        try? await AuthService.conditionalRefresh()
+        
+        do {
+            _ = try await NetworkManager.shared.delete(
+                path: "/items/user_locations/" + id,
+                parameters: nil
+            )
+            
+            return true
+        } catch let error {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    static func deleteFolders(ids: [String]) async -> Bool {
+        try? await AuthService.conditionalRefresh()
+        
+        do {
+            _ = try await NetworkManager.shared.delete(
+                path: "/items/user_locations",
+                parameters: [
+                    "keys": ids,
                 ]
             )
             
