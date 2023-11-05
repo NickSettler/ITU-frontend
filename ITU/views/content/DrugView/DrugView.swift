ede4d5ab14418509cd5e01078ee7a4cf23ccbd36
NickSettler <moiseevnikita14@gmail.com>
2023-11-05 01:50:33 +0100

    feat(drugs): add basic drug view

--- ITU/views/content/DrugView/DrugView.swift
+++ ITU/views/content/DrugView/DrugView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct DrugView: View {
+    @State var scrollOffset: CGPoint = .zero
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack {
+                VStack (alignment: .leading) {
+                    HStack {
+                        Text("General")
+                            .font(.callout)
+                            .textCase(.uppercase)
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 2)
+                    .background(.gray.opacity(0.4))
+                    .foregroundStyle(Color.black.opacity(0.5))
+                    
+                    VStack (spacing: 8) {
+                        VStack (spacing: 2) {
+                            Text("Strength")
+                                .font(.subheadline)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("PARACETAMOL AUROVITAS")
+                                .font(.headline)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        VStack (spacing: 2) {
+                            Text("Name")
+                                .font(.subheadline)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("PARACETAMOL AUROVITAS")
+                                .font(.headline)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 8)
+                    
+                    Spacer()
+                }
+            }
+        }
+        .navigationTitle("PARACETAMOL AUROVITAS")
+        .navigationBarTitleDisplayMode(.inline)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-    DrugView()
+    NavigationView {
+        DrugView()
+    }
 }
