cdd3196c08fdb3a400477bd05479b69cdc4222c5
NickSettler <moiseevnikita14@gmail.com>
2023-11-05 06:23:36 +0100

    feat(settings): add resetting refresh token on sign out

--- ITU/views/content/SettingsView/SettingsView.swift
+++ ITU/views/content/SettingsView/SettingsView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct SettingsView: View {
     @AppStorage(E_AUTH_STORAGE_KEYS.ACCESS_TOKEN.rawValue) private var accessToken: String?
+    @AppStorage(E_AUTH_STORAGE_KEYS.REFRESH_TOKEN.rawValue) private var refreshToken: String?
     
     var body: some View {
         Button {
             withAnimation {
                 accessToken = nil
+                refreshToken = nil
             }
         } label: {
             Text("Log out")
