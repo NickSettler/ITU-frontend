ef80689563c50cb8ae64837b07ecc232816e9a64
lenamarochkina <marochkina.lena@gmail.com>
2023-11-13 16:33:09 +0100

    feat: change card colors

--- ITU/components/DrugCard/DrugCard.swift
+++ ITU/components/DrugCard/DrugCard.swift
@@ -9,35 +9,62 @@ import SwiftUI
 
 struct DrugCard: View {
     @Binding var drug: Drug
-    
+    var expDate: Date
+
+    init(drug: Binding<Drug>, expDate: Date = Calendar.current.date(from: DateComponents(year: 2022, month: 12, day: 23)) ?? Date()) {
+        self._drug = drug
+        self.expDate = expDate
+    }
+
     var body: some View {
-        VStack(spacing: 0) {
+        let currentDate = Date()
+        let sixMonthsFromNow = Calendar.current.date(byAdding: .month, value: 6, to: currentDate) ?? Date()
+
+        let rectangleColor: (Color, Color, Color)
+        if expDate < currentDate {
+            rectangleColor = (Color.Quaternary300, Color.Quaternary400, Color.Quaternary600)
+        } else if expDate < sixMonthsFromNow {
+            rectangleColor = (Color.Secondary100, Color.Secondary200, Color.Secondary400)
+        } else {
+            rectangleColor = (Color.Primary100, Color.Primary200, Color.Primary500)
+        }
+
+        return VStack(spacing: 0) {
             Rectangle()
-                .fill(Color.colorPrimaryLight)
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [rectangleColor.0, rectangleColor.1]),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                )
                 .frame(height: 32)
-            
+                .overlay(
+                        Text("exp. date: 12/2023")
+                            .font(.caption)
+                            .foregroundColor(rectangleColor.2)
+                            .padding(.trailing, 8)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                                )
             VStack(spacing: 12) {
                 HStack {
                     Text(drug.name)
                         .font(.headline)
-                        .foregroundStyle(Color.textColorPrimary)
-                    
+                        .foregroundStyle(Color.Grey700)
+
                     Spacer()
-                    
+
                     Text("10 tablets")
                         .font(.caption)
-                        .foregroundStyle(Color.textColorSecondary)
+                        .foregroundStyle(Color.Grey400)
                 }
                 HStack {
                     Text("Tablets 100mg")
                         .font(.caption)
-                        .foregroundStyle(Color.textColorSecondary)
-                    
+                        .foregroundStyle(Color.Grey300)
+
                     Spacer()
-                    
-                    Text("exp. date: 12/2023")
-                        .font(.caption)
-                        .foregroundStyle(Color.textColorSecondary)
+
                 }
             }
             .padding(.horizontal, 20)
@@ -49,6 +76,7 @@ struct DrugCard: View {
         .shadow(color: Color.textColorPrimary.opacity(0.05), radius: 12, x: 0, y: 2)
     }
 }
+
 
 #Preview {
     CommonListView(size: .zero, safeArea: .init(.zero))
