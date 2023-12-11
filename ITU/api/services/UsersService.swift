db5e487b33d509fc7a96e02fb7cc96074ece51b5
NickSettler <moiseevnikita14@gmail.com>
2023-12-12 00:00:19 +0100

    fix(api): add selecting 3 times nested fields when getting current user

--- ITU/api/services/UsersService.swift
+++ ITU/api/services/UsersService.swift
@@ -13,11 +13,29 @@ struct UsersService {
         
         do {
             let data = try await NetworkManager.shared.get(
-                path: "/users/me",
+                path: "/users/me?fields=*.*.*",
                 parameters: nil
             )
             
             let result: ApiSuccessResponse<User> = try NetworkAPI.parseData(data: data)
+            
+            return result
+        } catch let error {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    static func search(query: String) async -> ApiSuccessResponse<[User]>? {
+        try? await AuthService.conditionalRefresh()
+        
+        do {
+            let data = try await NetworkManager.shared.get(
+                path: "/users?filter={ \"email\": { \"_contains\": \"\(query)\" }}",
+                parameters: nil
+            )
+            
+            let result: ApiSuccessResponse<[User]> = try NetworkAPI.parseData(data: data)
             
             return result
         } catch let error {
