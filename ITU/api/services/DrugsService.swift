3c426190b019fee01411e48864ae1ae982806792
NickSettler <moiseevnikita14@gmail.com>
2023-12-15 01:40:51 +0100

    feat(api): add new drug api methods

--- ITU/api/services/DrugsService.swift
+++ ITU/api/services/DrugsService.swift
@@ -59,6 +59,74 @@ struct DrugsService {
             return false
         }
     }
+    
+    static func updateDrug(id: Int, updatedDrug: Drug) async -> ApiSuccessResponse<Drug>? {
+        try? await AuthService.conditionalRefresh()
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = .current
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let formattedDate = dateFormatter.string(from: updatedDrug.expiration_date)
+        
+        guard let drugForm = updatedDrug.form else {
+            return nil
+        }
+
+        do {
+            let data = try await NetworkManager.shared.patch(
+                path: "/items/user_drugs/\(id)",
+                parameters: [
+                    "name": updatedDrug.name,
+                    "strength": updatedDrug.strength ?? "",
+                    "form": drugForm.form,
+                    "count": updatedDrug.count,
+                    "expiration_date": formattedDate
+                ]
+            )
+            
+            let result: ApiSuccessResponse<Drug> = try NetworkAPI.parseData(data: data)
+            return result
+        } catch let error {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    static func moveDrug(_ id: Int, folderID: String) async -> Bool {
+        try? await AuthService.conditionalRefresh()
+        
+        do {
+            _ = try await NetworkManager.shared.patch(
+                path: "/items/user_drugs/\(id)",
+                parameters: [
+                    "location": folderID
+                ]
+            )
+            
+            return true
+        } catch let error {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    static func deleteDrug(_ id: Int) async -> Bool {
+        try? await AuthService.conditionalRefresh()
+        
+        do {
+            _ = try await NetworkManager.shared.patch(
+                path: "/items/user_drugs/\(id)",
+                parameters: [
+                    "status": "DELETED"
+                ]
+            )
+            
+            return true
+        } catch let error {
+            print(error.localizedDescription)
+            return false
+        }
+    }
 
     static func getUserDrug(_ id: Int) async -> ApiSuccessResponse<GetUsersDrugResponse>? {
         try? await AuthService.conditionalRefresh()
